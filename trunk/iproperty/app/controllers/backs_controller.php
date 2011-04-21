df17b083f982487120fea6fe64ecee9afa0d8439
mafuyu9@gmail.com <mafuyu9@gmail.com@d3e3d31a-9210-a941-e292-be2b8c711f4a>
2011-04-21 04:19:36 +0000

    replace_new

---
+++ trunk/iproperty/app/controllers/backs_controller.php
@@ -0,0 +1,54 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+class BacksController extends AppController{
+    var $uses = 'Directorys';
+
+    function index(){
+        $this->data = $this->Directorys->find('all');
+    }
+
+    function insert(){
+//        if(empty($this->data)){
+//            //echo $this->redirect('/backs/insert');
+//            //echo 'debug empty';
+//           //$this->Directorys->create();
+//        }
+//        else{
+//
+//               if($this->Directorys->save($this->data)){
+//                   $this->Session->setFlash('เพิ่มข้อมลแล้วู');
+//                   $this->redirect('/backs/index');
+//               }
+//              else{
+//                  $this->Session->setFlash('ผิดพลาด');
+//              }
+
+           
+            //echo 'debug not empty';
+            //$this->render('view');
+            $this->render('view','default');
+    }
+    function save(){
+        if(!$this->Directorys->save($this->data)){
+            $this->setAction('edit',$this->data['Directorys']['id']);
+        }else{
+            $this->setAction('index');
+        }
+//        $this->Directorys->save($this->data);
+//        $this->redirect('/backs/index');
+    }
+    function edit($id = null){
+        $this->data = $this->Directorys->findbyid($id);
+        $this->render('view','default');
+    }
+    function delete($id){
+        $this->Directorys->delete($id);
+        $this->setAction('index');
+        //$this->redirect('/backs/index');
+    }
+    
+}
+?>
---
+++ trunk/iproperty/app/views/webs/index.ctp
@@ -0,0 +1,19 @@
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<center>
+<?php
+    echo $html->image('images/logo.jpg');
+?>
+    <br />กรุณารอสักครู่...
+</center>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+
